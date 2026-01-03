5dd7e61a0f133c4acabd86c6b281243be8c81ee3
Kahoul Abd el Madjid <105085145+Abd-el-madjid@users.noreply.github.com>
2026-01-03 16:32:26 +0100

    valid commit message

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -6,38 +6,44 @@ export const getCategoryColors = (color: string): ColorMapping => {
       dark: 'rgba(6, 182, 212, 1)',
       light: 'rgba(8, 145, 178, 1)',
       shadowDark: 'rgba(6, 182, 212, 0.3)',
-      shadowLight: 'rgba(6, 182, 212, 0.2)',
+      shadowLight: 'rgba(6, 182, 212, 0.2)'
     },
     purple: {
       dark: 'rgba(168, 85, 247, 1)',
       light: 'rgba(147, 51, 234, 1)',
       shadowDark: 'rgba(168, 85, 247, 0.3)',
-      shadowLight: 'rgba(168, 85, 247, 0.2)',
+      shadowLight: 'rgba(168, 85, 247, 0.2)'
     },
     green: {
       dark: 'rgba(16, 185, 129, 1)',
       light: 'rgba(13, 148, 136, 1)',
       shadowDark: 'rgba(16, 185, 129, 0.3)',
-      shadowLight: 'rgba(16, 185, 129, 0.2)',
+      shadowLight: 'rgba(16, 185, 129, 0.2)'
     },
     orange: {
       dark: 'rgba(251, 146, 60, 1)',
       light: 'rgba(239, 68, 68, 1)',
       shadowDark: 'rgba(251, 146, 60, 0.3)',
-      shadowLight: 'rgba(251, 146, 60, 0.2)',
+      shadowLight: 'rgba(251, 146, 60, 0.2)'
     },
     blue: {
       dark: 'rgba(59, 130, 246, 1)',
       light: 'rgba(79, 70, 229, 1)',
       shadowDark: 'rgba(59, 130, 246, 0.3)',
-      shadowLight: 'rgba(59, 130, 246, 0.2)',
+      shadowLight: 'rgba(59, 130, 246, 0.2)'
     },
     violet: {
       dark: 'rgba(139, 92, 246, 1)',
       light: 'rgba(124, 58, 237, 1)',
       shadowDark: 'rgba(139, 92, 246, 0.3)',
-      shadowLight: 'rgba(139, 92, 246, 0.2)',
+      shadowLight: 'rgba(139, 92, 246, 0.2)'
     },
+    pink: {
+      dark: 'rgba(236, 72, 153, 1)',
+      light: 'rgba(244, 114, 182, 1)',
+      shadowDark: 'rgba(236, 72, 153, 0.3)',
+      shadowLight: 'rgba(244, 114, 182, 0.2)'
+    }
   };
 
   return colorMap[color] || colorMap.blue;
