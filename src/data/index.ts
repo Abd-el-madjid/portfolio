5519648077b1c819eb4ab24b0d98beb5aaad4f62
Abd el Madjid Kahoul <105085145+Abd-el-madjid@users.noreply.github.com>
2026-01-04 19:17:55 +0100

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,9 +1,14 @@
 import projectsJson from './projects.json';
 import { CATEGORIES } from './categories';
 import { Project } from '@/types';
-import { getProjectMainImage } from '@/lib/projectImages';
+import { getProjectImages, getProjectMainImage } from '@/lib/projectImages';
 
-export const projects: Project[] = projectsJson.map((project) => ({
-  ...project,
-  image: getProjectMainImage(project.id),
-}));
+export const projects: Project[] = projectsJson.map(project => {
+  const images = getProjectImages(project.id);
+
+  return {
+    ...project,
+    image: images[0] ?? '',
+    images
+  };
+});
