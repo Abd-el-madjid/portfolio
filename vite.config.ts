5190592697387cfda85e5d14a860e1561a1caae7
Kahoul Abd el Madjid <105085145+Abd-el-madjid@users.noreply.github.com>
2026-01-05 03:13:01 +0100

    update chunking algorithm

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 // vite.config.ts
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -12,17 +12,17 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Vendor chunk for React, ReactDOM, Router
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'vendor-react';
-            }
-            return 'vendor';
+       manualChunks(id) {
+        if (id.includes('node_modules')) {
+          if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+            return 'vendor-react';
           }
-        },
+          return 'vendor';
+        }
+      }
+
       },
     },
-    chunkSizeWarningLimit: 1000, // Avoid warnings for large chunks
+    chunkSizeWarningLimit: 1000, // optional, avoid warnings
   },
-});
+})
