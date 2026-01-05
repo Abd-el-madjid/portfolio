1cfdb060c708c94ae26c376650d5d0d9dcd3718e
Abd el Madjid Kahoul <105085145+Abd-el-madjid@users.noreply.github.com>
2026-01-05 03:15:52 +0100

    Refactor vite.config.ts for improved readability

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,17 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   build: {
-    rollupOptions: {
-      output: {
-       manualChunks(id) {
-        if (id.includes('node_modules')) {
-          if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-            return 'vendor-react';
-          }
-          return 'vendor';
-        }
-      }
-
-      },
-    },
-    chunkSizeWarningLimit: 1000, // optional, avoid warnings
+    chunkSizeWarningLimit: 1000, // optional, avoid warnings for large chunks
   },
-})
+});
